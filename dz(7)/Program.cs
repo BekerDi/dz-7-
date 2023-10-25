a63e795423e6f80dbaa277792d3c092d7f8ceaac
BekerDi <d.beker.04@mail.ru>
2023-10-25 23:32:37 +0400

    Добавьте файлы проекта.

---
+++ dz(7)/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+
+namespace dz2110
+{
+
+    public enum AccountType
+    {
+        Checking,
+        Savings
+    }
+
+    public class BankAccount
+    {
+        private static int accountCounter = 0;
+
+        public int AccountNumber { get; private set; }
+        public decimal Balance { get; private set; }
+        public AccountType Type { get; private set; }
+
+        public BankAccount(AccountType type)
+        {
+            accountCounter++;
+            AccountNumber = accountCounter;
+            Type = type;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            Console.WriteLine($"Положено {amount} на счет {AccountNumber}");
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Снято {amount} со счета {AccountNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Недостаточно средств на счете {AccountNumber}. Невозможно снять {amount}");
+            }
+        }
+
+        public void DepositFromUser()
+        {
+            Console.Write($"Введите сумму для внесения на счет {AccountNumber}: ");
+            decimal depositAmount;
+            while (!decimal.TryParse(Console.ReadLine(), out depositAmount))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+                Console.Write($"Введите сумму для внесения на счет {AccountNumber}: ");
+            }
+            Deposit(depositAmount);
+        }
+
+        public void Transfer(BankAccount destinationAccount, decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Withdraw(amount);
+                destinationAccount.Deposit(amount);
+                Console.WriteLine($"Переведено {amount} со счета {AccountNumber} на счет {destinationAccount.AccountNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Недостаточно средств на счете {AccountNumber}. Невозможно перевести {amount}");
+            }
+        }
+    }
+    public static class Extensions
+    {
+        public static string Reverse(this string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+    public static class Extensions1
+    {
+        public static bool ImplementsIFormattable(this object obj)
+        {
+            return obj is IFormattable;
+        }
+    }
+
+
+    public class Example
+    {
+        //Упражнение 8.1
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount(AccountType.Checking);
+            BankAccount account2 = new BankAccount(AccountType.Savings);
+            account1.Deposit(1000);
+            Console.Write("Введите сумму для снятия со счета 1: ");
+            decimal withdrawalAmount1;
+            while (!decimal.TryParse(Console.ReadLine(), out withdrawalAmount1))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+                Console.Write("Введите сумму для снятия со счета 1: ");
+            }
+            account1.Withdraw(withdrawalAmount1);
+
+            account2.Deposit(2000);
+            Console.Write("Введите сумму для снятия со счета 2: ");
+            decimal withdrawalAmount2;
+            while (!decimal.TryParse(Console.ReadLine(), out withdrawalAmount2))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+                Console.Write("Введите сумму для снятия со счета 2: ");
+            }
+            account2.Withdraw(withdrawalAmount2);
+
+            account1.DepositFromUser();
+            account2.DepositFromUser();
+
+            Console.Write("Введите сумму для перевода со счета 1 на счет 2: ");
+            decimal transferAmount;
+            while (!decimal.TryParse(Console.ReadLine(), out transferAmount))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+                Console.Write("Введите сумму для перевода со счета 1 на счет 2: ");
+            }
+            account1.Transfer(account2, transferAmount);
+
+            Console.ReadKey();
+
+
+
+            // Упражнение 8.2
+            Console.WriteLine("Введите строку:");
+            string inputStr = Console.ReadLine();
+            string reversedStr = inputStr.Reverse();
+            Console.WriteLine("Перевернутая строка: " + reversedStr);
+
+            // Упражнение 8.3
+            Console.WriteLine("Введите имя файла:");
+            string fileName = $"../../{Console.ReadLine()}";
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("Файла не существует.");
+                Console.WriteLine("Нажмите любую клавишу");
+                Console.ReadKey();
+                Environment.Exit(0);
+
+
+            }
+            else
+            {
+
+                string fileContent = File.ReadAllText(fileName);
+                string upperCaseContent = fileContent.ToUpper();
+                File.WriteAllText("../../output.txt", upperCaseContent);
+                Console.WriteLine("Содержимое файла записано в выходной файл.");
+            }
+            //Упражнение 8.4
+            int number = 10;
+            string str = "Hello";
+
+            Console.WriteLine("number implements IFormattable: " + number.ImplementsIFormattable());
+            Console.WriteLine("str implements IFormattable: " + str.ImplementsIFormattable());
+            Console.ReadKey();
+        }
+    }
+
+}
+
+
+
+
+
---
+++ tumakov2110/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Employee
+{
+    public string Name { get; set; }
+    public Employee DirectManager { get; set; }
+
+    public Employee(string name, Employee directManager)
+    {
+        Name = name;
+        DirectManager = directManager;
+    }
+
+    public bool TakeTask(string task, Employee assignee)
+    {
+        if (assignee == this || assignee.DirectManager == this)
+        {
+            Console.WriteLine($"{Name} дает задачу \"{task}\" {assignee.Name}");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"{Name} не может дать задачу {assignee.Name}");
+            return false;
+        }
+    }
+}
+
+// Класс для генерального директора
+class CEO : Employee
+{
+    public CEO(string name) : base(name, null) { }
+}
+
+// Класс для финансового директора
+class CFO : Employee
+{
+    public CFO(string name, Employee directManager) : base(name, directManager) { }
+}
+
+// Класс для директора по автоматизации
+class AutomationDirector : Employee
+{
+    public AutomationDirector(string name, Employee directManager) : base(name, directManager) { }
+}
+
+// Класс для главного бухгалтера
+class ChiefAccountant : Employee
+{
+    public ChiefAccountant(string name, Employee directManager) : base(name, directManager) { }
+}
+
+// Класс для начальника отдела информационных технологий
+class ITManager : Employee
+{
+    public ITManager(string name, Employee directManager) : base(name, directManager) { }
+}
+
+// Класс для заместителя начальника отдела информационных технологий
+class ITDeputyManager : Employee
+{
+    public ITDeputyManager(string name, Employee directManager) : base(name, directManager) { }
+}
+
+// Класс для сотрудников отдела системщиков
+class NetworkEngineer : Employee
+{
+    public NetworkEngineer(string name, Employee directManager) : base(name, directManager) { }
+}
+
+// Класс для сотрудников отдела разработчиков
+class Developer : Employee
+{
+    public Developer(string name, Employee directManager) : base(name, directManager) { }
+}
+public class Song
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public Song Prev { get; set; }
+
+    public Song(string name, string author, Song prev)
+    {
+        Name = name;
+        Author = author;
+        Prev = prev;
+    }
+
+    public string Title()
+    {
+        return Name + " ; " + Author;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Song other = (Song)obj;
+        return Name == other.Name && Author == other.Author;
+    }
+}
+
+public class Example
+{
+    static void Main(string[] args)
+    {
+        List<Song> songs = new List<Song>();
+
+        Song song1 = new Song("Она не твоя", "Григорий Лепс", null);
+        Song song2 = new Song("Она не твоя", "Григорий Лепс", song1);
+        Song song3 = new Song("Прсто друг", "Чвай вдвоем", song2);
+        Song song4 = new Song("Я буду", "Лоя", song3);
+
+        songs.Add(song1);
+        songs.Add(song2);
+        songs.Add(song3);
+        songs.Add(song4);
+
+        foreach (Song song in songs)
+        {
+            Console.WriteLine(song.Title());
+        }
+
+        if (song1.Equals(song2))
+        {
+            Console.WriteLine("Песни 1 и 2 одинаковы");
+        }
+        else
+        {
+            Console.WriteLine("Песни 1 и 2 разные");
+        }
+
+        // Создание иерархии сотрудников
+        CEO semyon = new CEO("Семен");
+        CFO rashid = new CFO("Рашид", semyon);
+        AutomationDirector ilham = new AutomationDirector("О Ильхам", semyon);
+        ChiefAccountant lukas = new ChiefAccountant("Лукас", rashid);
+        ITManager orkadiy = new ITManager("Оркадий", ilham);
+        ITDeputyManager volodya = new ITDeputyManager("Володя", ilham);
+        NetworkEngineer ilshat = new NetworkEngineer("Ильшат", orkadiy);
+        NetworkEngineer ivanych = new NetworkEngineer("Иваныч", orkadiy);
+        Developer sergey = new Developer("Сергей", orkadiy);
+        Developer lyaysan = new Developer("Ляйсан", orkadiy);
+        NetworkEngineer ilya = new NetworkEngineer("Илья", ivanych);
+        NetworkEngineer vitia = new NetworkEngineer("Витя", ivanych);
+        NetworkEngineer zhenya = new NetworkEngineer("Женя", ivanych);
+        Developer marat = new Developer("Марат", lyaysan);
+        Developer dina = new Developer("Дина", lyaysan);
+        Developer ildar = new Developer("Ильдар", lyaysan);
+        Developer anton = new Developer("Антон", lyaysan);
+
+        // Примеры назначения задач
+        semyon.TakeTask("Задача 1", rashid);
+        rashid.TakeTask("Задача 2", lukas);
+        ilham.TakeTask("Задача 3", orkadiy);
+        orkadiy.TakeTask("Задача 4", ilshat);
+        lyaysan.TakeTask("Задача 5", marat);
+        sergey.TakeTask("Задача 6", dina);
+
+        //Задание 
+        Console.WriteLine("Введите название файла");
+        string nameFile = Console.ReadLine();
+        string[] lines = File.ReadAllLines(nameFile);
+
+        using (StreamWriter writer = new StreamWriter("output.txt"))
+        {
+            foreach (string line in lines)
+            {
+                string email = GetEmail(line);
+
+                writer.WriteLine(email);
+            }
+        }
+
+        Console.WriteLine("Готово!");
+    }
+
+    static string GetEmail(string line)
+    {
+        int separatorIndex = line.IndexOf('#');
+
+
+        if (separatorIndex == -1)
+            return "";
+
+
+        string email = line.Substring(separatorIndex + 1).Trim();
+
+        return email;
+
+        Console.ReadKey();
+    }
+
+}
+
+
+
+
+
+
+
